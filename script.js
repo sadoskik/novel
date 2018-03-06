a4acee6f814863450e1b6640b8deb6ce2b48ec53
sadoskik <sadoskik@gmail.com>
2018-03-05 22:34:55 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+var facts = [
+  {year:"Massachusetts", event:"What state does this book take place in?"},
+  {year:"H.P. Lovecraft" , event:"Who is the author of our book?"},
+  {year:"Arkham", event:"To what city is the speaker ultimately trying to travel?"},
+  {year:"Prohibition", event:"What era does this book take place in?"},
+  
+  {year:"Devil's Reef", event:"What's the name of the ocean-based mass that causes the strife?"},
+  {year:"Antiquarian", event:"Lovecraft felt he was this and many of his characters held this hobby"},
+  {year:"Cosmocism", event:"Lovecraft started this movement"},
+  {year:"Xenophobia", event:"This theme is shown by the main character's innate hatred of the townspeople's characteristics"}
+  
+  
+];
