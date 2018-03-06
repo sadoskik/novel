7e93c4598a1e95c9106381a27c010e07d060748e
sadoskik <sadoskik@gmail.com>
2018-03-05 23:09:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var facts = [
   {year:"Massachusetts", event:"What state does this book take place in?"},
-  {year:"H.P. Lovecraft", event:"Who is the author of our book?"},
+  {year:"H.P. Lovecraft", event:"Who is the author of our book? (Include initials ex. [A.B.])"},
   {year:"Arkham", event:"To what city is the speaker ultimately trying to travel?"},
   {year:"Prohibition", event:"What era does this book take place in?"},
   {year:"Devil's Reef", event:"What's the name of the ocean-based mass that causes the strife?"},
-  {year:"Antiquarian", event:"Lovecraft felt he was this and many of his characters held this hobby"},
-  {year:"Cosmocism", event:"Lovecraft started this movement"},
-  {year:"Xenophobia", event:"This theme is shown by the main character's innate hatred of the townspeople's characteristics"},
-  {year:"Non-Euclidean", event:"This form of geometry is mentioned by Lovecraft. A kind of mystical and illogical space"}
+  {year:"Antiquarian", event:"Lovecraft was this and many of his characters held this hobby as well"},
+  {year:"Cosmocism", event:"Lovecraft started this movement. Think Doctor Strange"},
+  {year:"Xenophobia", event:"This theme is shown by the townspeople in their innate hatred of the Innsmouthers"},
+  {year:"Non-Euclidean", event:"This form of geometry is mentioned by Lovecraft. A kind of mystical and illogical space"},
+  {year:"Yes", event:"Was this format fun and enlightening? (The only correct response is Yes)"}
 ];
